50fa0ac26ac39b7c1eee0ede99be18f7f47cad5c
Jesus Romero <elherzt@gmail.com>
2015-12-30 12:57:14 -0600

    added information page for companies

--- config/routes.rb
+++ config/routes.rb
@@ -10,4 +10,6 @@ Rails.application.routes.draw do
   root to: 'visitors#index'
   devise_for :users
   resources :users
+  get 'funcionamiento' => "visitors#operation"
+  get 'empresas' => "visitors#for_companies"
 end
